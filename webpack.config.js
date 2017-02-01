e89d87e37aa69c77f0b01d69e2bbf2e8231bd6fb
Andrey <mr.akovalenko@gmail.com>
2017-02-01 16:19:05 +0000

    fix both commands server & build

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,21 @@
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+const helpers = require('./helpers');
+const CheckerPlugin = require('awesome-typescript-loader').CheckerPlugin;
+
+const ENV = process.env.ENV = process.env.NODE_ENV = 'development';
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 3000;
+const METADATA = {
+  host: HOST,
+  port: PORT,
+  ENV: ENV,
+  baseUrl: '/',
+  isDevServer: helpers.isWebpackDevServer()
+
+};
+console.log('--------->', helpers.root('src'), METADATA);
+
 
 module.exports = {
   entry: {
@@ -8,8 +24,13 @@ module.exports = {
       app: './src/index'
   },
   output: {
-    filename: '[chunkhash].[name].js',
-    path: './dist'
+    filename: '[name].bundle.js',
+    path: helpers.root('dist'),
+
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+    modules: [ 'node_modules', 'src'],
   },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({
